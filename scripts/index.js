f41a4ea1cc68707ae04efe1b4bd6185aebe8392e
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-04-08 08:23:06 -0300

    remove:Static html; feat:apicall;

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-//Init com apai call
+function getApiGames() {
+  const ajax = new XMLHttpRequest();
+  ajax.open("GET", "/games.json", true);
+  ajax.send();
+
+  const successCall = () => {
+    if (ajax.readyState === 4 && ajax.status === 200) {
+      console.log(ajax.responseText);
+    }
+  };
+
+  ajax.addEventListener("readystatechange", successCall, false);
+}
+getApiGames();
