136cc70d47490a2e1b7c1373d163954e16b83828
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-04-08 10:35:03 -0300

    feat:buttonApi game type

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,61 @@
-function getApiGames() {
-  const ajax = new XMLHttpRequest();
-  ajax.open("GET", "/games.json", true);
-  ajax.send();
+let game = [];
+let gameRange = [];
+let cart = [];
 
-  const successCall = () => {
-    if (ajax.readyState === 4 && ajax.status === 200) {
-      console.log(ajax.responseText);
-    }
-  };
+//Html variables
 
-  ajax.addEventListener("readystatechange", successCall, false);
-}
-getApiGames();
+const $betTypeRange = document.querySelector(".grid-bet-container-range"); //ok
+
+const api = {
+  getApiGames() {
+    const ajax = new XMLHttpRequest();
+    ajax.open("GET", "/games.json", true);
+    ajax.send();
+
+    const successCall = () => {
+      try {
+        if (ajax.readyState === 4 && ajax.status === 200) {
+          //JSON parse, copy all sub propries in OBJ.
+          game.push(JSON.parse(ajax.responseText).types);
+          htmlRender.gameTypes();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    ajax.addEventListener("readystatechange", successCall, false);
+  },
+};
+
+//Onlick do button.
+const handleEvents = {
+  switchGameMode() {
+    console.log(`hi`); //ReferenceError
+  },
+};
+
+//Render gametypes button
+const htmlRender = {
+  gameTypes() {
+    const $betTypes = document.querySelector(".grid-bet-container-button");
+    game[0].map((game) => {
+      $betTypes.innerHTML += `
+      <button
+        onclick="handleEvents.switchGameMode()"
+        style="color:${game.color};border:2px solid ${game.color}" >
+        ${game.type}
+      </button>
+      `;
+    });
+  },
+};
+
+const app = {
+  init() {
+    api.getApiGames();
+    console.log(game);
+  },
+};
+
+app.init();
